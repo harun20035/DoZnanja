27c05423c0f5a51fa125ea98b19ed3a35f865453
Davud <davudfazlic04@gmail.com>
2025-05-09 03:33:27 +0200

    Registracija ende

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI
 from database import database
 from init_db import create_db_and_tables
 from controllers import user_controller
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Dopušta samo ovom URL-u
+    allow_credentials=True,
+    allow_methods=["*"],  # Dopušta sve HTTP metode (GET, POST, PUT, DELETE, itd.)
+    allow_headers=["*"],  # Dopušta sve zaglavlja
+)
 
 
 
@@ -22,5 +32,6 @@ async def shutdown():
 async def test_connection():
     result = await database.fetch_one("SELECT NOW();")
     return {"vreme_na_bazi": result[0]}
+
 
 app.include_router(user_controller.router, prefix="/users", tags=["Users"])
